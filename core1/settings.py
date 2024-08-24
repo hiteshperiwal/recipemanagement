1ef3fb18422c7f5c9c489948405e8f45386c042b
hiteshperiwal <hiteshperiwal18@gmail.com>
2024-08-24 18:12:28 +0530

    v2

--- core1/settings.py
+++ core1/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lz)3(jxvmppv$#zptq3kbg7v+w%pf^&u3g0_-)@x^-5spcbqiw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vercel.app']
 
 EXTERNAL_APP=['vege']
 # Application definition
