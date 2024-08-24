c8b8f65d4ec58f451cdadf68c22675e67448c115
hiteshperiwal <hiteshperiwal18@gmail.com>
2024-08-24 19:15:27 +0530

    vercel settings

--- recipemanagement/settings.py
+++ recipemanagement/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build',"static")
 
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
+        os.path.join(BASE_DIR, '\static'),
     ]
 
 # Media files (user uploaded content)
