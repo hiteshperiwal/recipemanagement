b2536f75d22d3dd332f753abf88bd67e66c183ed
hiteshperiwal <hiteshperiwal18@gmail.com>
2024-08-26 21:44:47 +0530

    build_files

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 pip install -r requirements.txt
 
-pyton manange.py makemigrations --noinput
-pytho manage.py migrate --noinput
+python manange.py makemigrations --noinput
+python manage.py migrate --noinput
 
 python manage.py collectstatic --noinput
